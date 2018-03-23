c54dec1c17bb1d4ddaba135f60c5d4fbeac03cf2
Oli Bullen <d33con@gmail.com>
2018-03-23 16:50:40 +0000

    update bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,12 +19,15 @@ const AboutPage = () => (
     </ul>
     <div className="skills-title">About Me</div>
     <div className="bio">
-      <p>I'm a Front End Developer from the UK.</p>
       <p>
-        I've been developing and learning for around 2 years. I love creating
-        modern, responsive websites and applications keeping up wth the latest
-        trends. I have recently returned from living in Japan and am looking for
-        my first professional development position.
+        I have recently returned to the UK after 4 years living in  Japan. Over
+        the last 2 years I have been self-learning web  development and am keen
+        to take the first step in a new  career. I am skilled in modern
+        responsive HTML, CSS and  Javascript. Focusing on React lately, I
+        actively seek out new technologies and keep up to date with the latest
+        trends and advancements. I am constantly looking to improve on the
+        skills I have learned so far whilst gaining new knowledge and I would
+        welcome the chance to do this in a professional environment.
       </p>
       <p>
         In my spare time I love listening to and occasionally producing music,
