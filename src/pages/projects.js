8e119917ae1a1c542528a2fae6188d456e181842
Oli Bullen <d33con@gmail.com>
2018-04-06 17:03:35 +0100

    project descriptions

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,14 +16,14 @@ const ProjectsPage = () => (
     <Card
       image={tracklistah}
       title="Tracklistah"
-      description="Mix tracklisting app built with React"
+      description="A mix tracklister and custom audio player using the Discogs API. Built with React"
       webLink="https://d33con.github.io/tracklistr"
       githubLink="https://github.com/d33con/tracklistr"
     />
     <Card
       image={crimean}
       title="Crimean War Casualties"
-      description="Sunburst visualisation built with react-vis"
+      description="A sunburst visualisation built with react-vis"
       webLink="https://d33con.github.io/crimean-war-casualties/"
       githubLink="https://github.com/d33con/crimean-war-casualties"
     />
@@ -37,28 +37,28 @@ const ProjectsPage = () => (
     <Card
       image={euros}
       title="Euro 2016 team & player cards"
-      description="CSS & jQuery"
+      description="Group and player cards designed using CSS & jQuery"
       webLink="https://codepen.io/d33con/full/xOwQmN/"
       githubLink="https://github.com/d33con/euro-2016-team-player-cards"
     />
     <Card
       image={blv}
       title="Buscate La Vida"
-      description="UK placement company site built with Bootstrap"
+      description="Bootstrap site built for a UK student placement company"
       webLink="https://www.buscatelavida.co.uk"
       githubLink=""
     />
     <Card
       image={d3}
       title="D3 Visualisations"
-      description="Collection of data visualisations built with D3.js"
+      description="A collection of data visualisations built with D3.js"
       webLink="https://codepen.io/collection/nWxkKw/"
       githubLink=""
     />
     <Card
       image={pomodoro}
       title="Pomodoro Timer"
-      description="Built with React"
+      description="A work / break timer built with React"
       webLink="https://d33con.github.io/react-pomodoro-timer/"
       githubLink="https://github.com/d33con/react-pomodoro-timer"
     />
